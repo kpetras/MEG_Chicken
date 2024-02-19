d87216790b663736f8de5e4d8817f813b3a852e2
Coline Haro <coline.haro@live.fr>
2024-02-19 16:58:59 +0100

    track responses in a list

--- test.py
+++ test.py
@@ -6,19 +6,17 @@ import matplotlib.pyplot as plt
 
 # %%
 # Load EEG data
-raw = mne.io.read_raw_fif('03TPZ5_session2_run01.fif', preload=True, allow_maxshield=True)
-print(raw.info)
+path = '/Users/coline/Desktop/Internship/03TPZ5_session2_run01.fif'
+raw = mne.io.read_raw_fif(path, preload=True, allow_maxshield=True)
 
 # %%
 # Downsample the data 
 raw_resampled = raw.copy().resample(250, npad="auto")
-print(raw_resampled.info)
 
 # %%
 # Add channels to the list of "bad datas" 
 channels_to_mark_as_bad = ['MEG0332','MEG1022','MEG1143','MEG1242','MEG1333','MEG1433','MEG2013','MEG2023','MEG2413','MEG2623','MEG0121','MEG1931']
 raw_resampled.info['bads'].extend(channels_to_mark_as_bad)
-print(raw_resampled.info)
 
 # %%
 # Define channels
@@ -28,10 +26,7 @@ good_channels = [ch for ch in all_channels if ch not in bad_channels]
 
 # %%
 # First block : assess a single channel 
-
-# Initialize the variables
-correct = 0
-incorrect = 0
+classification_results = []
 
 for i in range(10):
     # Select a random channel
@@ -55,12 +50,13 @@ for i in range(10):
     # Determine if the user's response is correct and provide feedback 
     if (channel_status == "bad" and selected_channel in bad_channels) or (channel_status == "good" and selected_channel in good_channels):
         print("Yes, you are right!")
-        correct += 1
+        classification_results.append(1)
     elif (channel_status == "bad" and selected_channel in good_channels) or (channel_status == "good" and selected_channel in bad_channels):
         print("No, you are wrong.")
-        incorrect += 1
+        classification_results.append(0)
     else:
         print("The channel status is not valid.")
 
-print("Number of correct responses for the first block:", correct)
-print("Number of incorrect responses for the first block:", incorrect)
+print("Classification results for the first block:", classification_results)
+
+
