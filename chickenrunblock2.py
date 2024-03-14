0dd311e22a1fd939eca9c4da3d232b1a845ddacf
Coline210 <158753552+Coline210@users.noreply.github.com>
2024-03-14 11:00:41 +0100

    load matlab data

---
+++ chickenrunblock2.py
@@ -0,0 +1,28 @@
+# %%
+from scipy.io import loadmat
+import mne
+import numpy as np
+
+# Load MATLAB file
+mat_data = loadmat('/Users/coline/Desktop/datamatlab/SO_02_00_S01_T00_compdat.mat')
+
+# Extract the data and channel names
+eeg_data = mat_data['dat'][0,0]['trial'][0,0]
+channel_names = [str(ch[0]) for ch in mat_data['dat'][0,0]['label']]
+
+# Define the sample rate
+sfreq = mat_data['dat'][0,0]['fsample'][0,0]  # Extract sample rate from the data
+
+# Create the info structure needed by MNE
+info = mne.create_info(channel_names, sfreq, ch_types='eeg')
+
+# Create the RawArray object
+raw = mne.io.RawArray(eeg_data, info)
+
+print(raw.info)
+print(raw._data.shape)
+”
+# Plot the raw data
+raw.plot(n_channels=42, duration=10)
+
+# %%
