76dc4896a7b0432357d37ddf22f428fd5c435968
Coline210 <158753552+Coline210@users.noreply.github.com>
2024-03-14 13:34:27 +0100

    plot ICA components

--- chickenrunblock2.py
+++ chickenrunblock2.py
@@ -1,6 +1,7 @@
 # %%
 from scipy.io import loadmat
 import mne
+from mne.preprocessing import ICA
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,8 +10,11 @@ import matplotlib.pyplot as plt
 mat_data = loadmat('/Users/coline/Desktop/datamatlab/SO_02_00_S01_T00_compdat.mat')
 
 # Extract the data and channel names
-eeg_data = mat_data['dat'][0,0]['trial'][0,0]/10000
+eeg_data = mat_data['dat'][0,0]['trial'][0,0]/ 1e6 
 channel_names = [str(ch[0]) for ch in mat_data['dat'][0,0]['label']]
+
+# Remove extra characters from channel names
+channel_names = [name.replace("['", "").replace("']", "") for name in channel_names]
 
 # Define the sample rate
 sfreq = mat_data['dat'][0,0]['fsample'][0,0]  # Extract sample rate from the data
@@ -25,6 +29,27 @@ print(raw.info)
 print(raw._data.shape)
 
 # Plot the raw data
-raw.plot(n_channels=42, duration=1)
+raw.plot(n_channels=42, duration=5)
 
+# Define a mapping from channel names to new channel types
+channel_types = {
+    'Lower VEOG': 'eog', 
+    'Upper VEOG': 'eog', 
+    'Left HEOG': 'eog', 
+    'Right HEOG': 'eog', 
+    'Offline reference': 'misc'
+}
+
+# Set the channel types
+raw.set_channel_types(channel_types)
+
+# Set the electrode montage
+raw.set_montage('standard_1005')  
+
+# Run ICA with runica method
+ica = mne.preprocessing.ICA(n_components=37, method='infomax')
+ica.fit(raw)
+
+# Plot components to review and identify artifacts
+ica.plot_components()
 # %%
