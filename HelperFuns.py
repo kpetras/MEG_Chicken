acdbd36adfc731b3c7bb019ea3e9353caf9017ab
LizzieYK <kongyue.liz@gmail.com>
2024-04-11 16:23:28 +0200

    Done!

--- HelperFuns.py
+++ HelperFuns.py
@@ -48,6 +48,95 @@ def monitor_bads(fig, answer,shared):
             # Check if the space key has been pressed
             if shared.get('space_pressed', False):
                 print("Space key pressed.")
+                
+                answer_set = set(answer)
+                current_bads_set = set(current_bads)
+
+                # New : Trouver les channels manquants en comparant les deux ensembles
+                missing_channels = answer_set - current_bads_set
+
+                if missing_channels:
+                    print(f"Missing channels: {', '.join(missing_channels)}")
+                else:
+                    print("All bad channels were correctly identified.")
+                break
+            
+            # print("Current bads: ", current_bads)  # Check if current_bads is updating
+
+            # Check if the length has changed
+            if len(current_bads) != len(previous_bads):
+                # print("Length of bads has changed.")
+
+                # Find which elements were added
+                added = set(current_bads) - set(previous_bads)
+                if added:
+                    print("Added: ", added)
+                    # check if added is in the answer
+                    added_in_answer = [item for item in added if item in answer]
+
+                    if added_in_answer:
+                        print("Correctly added: ", added_in_answer)
+                        #display_message("Good Job!", "green")
+                    else:
+                        print("Incorrectly added: ", added)
+                        #display_message("Incorrect! Try again", "red")
+
+                # Find which elements were removed
+                removed = set(previous_bads) - set(current_bads)
+                if removed:
+                    print("Removed: ", removed)
+                    # check if removed is in the answer
+                    removed_in_answer = [item for item in removed if item not in answer]
+                    if removed_in_answer:
+                        print("Correctly removed: ", removed_in_answer)
+                    else:
+                        print("Incorrectly removed: ", removed)
+
+            # Update previous_bads
+            previous_bads = current_bads.copy()
+
+            # Increment the counter
+            counter += 1
+
+            # Break the loop after 20 iterations
+            if counter >= 20:
+                break
+
+            # Wait for a short period of time before checking again
+            time.sleep(1)
+            
+        print("end loop")
+        
+    except Exception as e:
+        print("Error in thread: ", e)  # Check if there's an error in the thread
+        
+def monitor_ICs(ica, answer, shared):
+    # Initialize previous_bads to an empty list
+    previous_bads = []
+    
+    # Initialize a counter
+    counter = 0
+
+    try:
+        print("start loop")
+
+        while True:
+            # Get the current bads
+            current_bads = ica.exclude
+            # Check if the space key has been pressed
+            if shared.get('space_pressed', False):
+                print("Space key pressed.")
+                
+                answer_set = set(answer)
+                current_bads_set = set(current_bads)
+
+                # New : Trouver les channels manquants en comparant les deux ensembles
+                missing_ICs = answer_set - current_bads_set
+
+                if missing_ICs:
+                    print(f"Missing ICs: {', '.join(map(str, missing_ICs))}")
+                else:
+                    print("All bad ICs were correctly identified.")
                 break
 
             # print("Current bads: ", current_bads)  # Check if current_bads is updating
@@ -65,10 +154,10 @@ def monitor_bads(fig, answer,shared):
 
                     if added_in_answer:
                         print("Correctly added: ", added_in_answer)
-                        display_message("Good Job!", "green")
+                        #display_message("Good Job!", "green")
                     else:
                         print("Incorrectly added: ", added)
-                        display_message("Incorrect! Try again", "red")
+                        #display_message("Incorrect! Try again", "red")
 
                 # Find which elements were removed
                 removed = set(previous_bads) - set(current_bads)
--- chickenrun_ctrl_exp.py
+++ chickenrun_ctrl_exp.py
@@ -14,8 +14,9 @@ import HelperFuns as hf
 # %%
 # Load Laetitia's data and labels
 path  = 'data/'
-subj = '03TPZ5'
-ses = 'session2'
+# path = '/Users/elizabeth/Desktop/MEGChicken/'
+subj = 'FADM9A'
+ses = 'session1'
 run = 'run01.fif'
 fname = subj + '_' + ses + '_' + run
 fname_with_path = path + fname
@@ -25,6 +26,8 @@ raw = mne.io.read_raw_fif(fname_with_path, preload=True, allow_maxshield=True)
 from config import badC_EEG, badC_MEG, ICA_remove_inds
 badC_MEG_list = badC_MEG[subj][ses][run]
 badC_EEG_list = badC_EEG[subj][ses][run]
+ICA_remove_inds_list = ICA_remove_inds[subj][run]
+# or use ICA_remove_inds_concatRuns, but don't wanna bother with concatenation for now
 
 # %%
 # filtering
@@ -45,23 +48,6 @@ raw_filtered.filter(
     l_freq=None, h_freq=h_freq, fir_design="firwin", fir_window="hamming"
     )
 # raw_filtered.plot(n_channels=15, duration=2)
-
-# %%
-# prepare the epochs
-events = mne.find_events(
-    raw_filtered, stim_channel="STI101", shortest_event=5, min_duration=0.002,initial_event=True
-    )
-
-event_dict = {
-    "stand": 12,
-    "travIN": 22,
-    "travOut": 11,
-}
-
-epochs = mne.Epochs(raw_filtered, events, tmin=-1, tmax=3, event_id=event_dict, preload=True)
-# epochs["stand"].compute_psd(fmax= 80).plot(picks="meg", exclude="bads")
-
-epochs_decimated = epochs.decimate(2)  # decimate to 500 Hz
 
 # %%
 ####################
@@ -110,143 +96,58 @@ listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
 # %%
-
-# retrieve current selection of bad channels and Annotations
-# while the figure window is still open
-bads = fig.mne.info['bads']
-annotations = fig.mne.inst.annotations
-
-# now we need to know which channels are displayed in the figure
-# - using the thread thing ?
-# - or using the figure object ? don't know how to do that
-# - or pre-define the channels to be displayed, and monitoring the keyboard input all the time to calculate which channel is on the screen
-# see below:
-# raw.plot()
-# order: array of int
-# Order in which to plot data.
-# If the array is shorter than the number of channels, only the given channels are plotted.
-# If None (default), all channels are plotted. If group_by is 'position' or 'selection', the order parameter is used only for selecting the channels to be plotted.
-
-
-# next problem:
-# mne and tkinter are both not thread safe, so we cannot run them in parallel
-# - we can use the threading module to run the tkinter window in a separate thread
-# - we can use the multiprocessing module to run the mne functions in a separate process
-# - we find a thread-safe package for GUI
-
-# then we can use the tkinter window to display the instructions and the buttons
-
-# next, we need to record the user input and compare with the labels
-
-# finally, we need to analyze the results
-
-# %%
 ####################
-# ignore the following code for now
+# block for the ICA rejection
 ####################
+import matplotlib
+matplotlib.use('Qt5Agg')  # Make sure to use an interactive backend
 
-# display text instrcutions
-def thread_job():
-    print("Thread job started")
+# Do the ICA decomposition for each sensor type
+chs_type = 'eeg' # ['mag', 'grad', 'eeg']
+answer = ICA_remove_inds_list[chs_type]
 
-def main():
-    
-    added_thread = threading.Thread(target=thread_job)
-    added_thread.start()
-    
-if __name__ == "__main__":
-    main()
+# Fit ICA component
+ica = mne.preprocessing.ICA(n_components=50, random_state=42)
+ica.fit(raw_filtered, picks = chs_type)
 
-# %%
-# Create the main window
-root = tk.Tk()
-# Create a Label widget with the text message
-label = tk.Label(root, text="Your text message here")
-label.pack()
+n_components_per_page = 50 # number of components per page
 
-# Create the "good" and "bad" buttons
-good_button = tk.Button(root, text="Good", command=lambda: next_epoch('good'))
-bad_button = tk.Button(root, text="Bad", command=lambda: next_epoch('bad'))
+# Create a shared dictionary
+shared = {'space_pressed': False}
 
-# Add the buttons to the window
-good_button.pack()
-bad_button.pack()
+# Create a new thread that runs the monitor_bads function
+thread = threading.Thread(target=hf.monitor_ICs, args=(ica, answer, shared))
 
-# Current epoch index
-current_epoch = 0
+# Start the new thread
+thread.start()
 
-# Function to go to the next epoch
-def next_epoch(button_clicked):
-    global current_epoch
-    current_epoch += 1
-    # Update the epoch plot
-    epochs_decimated["stand"][current_epoch].plot(
-        events=events,
-        event_id=event_dict,
-        n_epochs=1,
-        n_channels=1
-    )
-    # Update the label text
-    label.config(text=f"Button {button_clicked} clicked. Now showing epoch {current_epoch}")
+# Listen for space key press
+def on_press(key):
+    if key == keyboard.Key.space:
+        shared['space_pressed'] = True
 
-# Start the tkinter window in a separate thread
-threading.Thread(target=root.mainloop).start()
+# Start listening for key press
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
-# Plot the first epoch
-epochs_decimated["stand"][current_epoch].plot(
-    events=events,
-    event_id=event_dict,
-    n_epochs=1,
-    n_channels=1
-)
-
-current_epoch = 1
-new_channel = "MEG0111"
-epochs_decimated["stand"][current_epoch].plot(
-    events=events,
-    event_id=event_dict,
-    n_epochs=1,
-    n_channels=1
-    )
-
-# %%
-# Function to create and start the tkinter window
-def create_tkinter_window():
-    root = tk.Tk()
-    label = tk.Label(root, text="Your text message here")
-    label.pack()
-    root.mainloop()
-
-# Start the tkinter window in a separate thread
-threading.Thread(target=create_tkinter_window).start()
-
-# Create the main window
-root = tk.Tk()
-# Create a Label widget with the text message
-label = tk.Label(root, text="Your text message here")
-label.pack()
-# Function to update the tkinter window
-def update_tkinter_window(new_text):
-    # Update the label text
-    label.config(text=new_text)
-    # Redraw the window
-    root.update()
-
-# Start the tkinter window in a separate thread
-threading.Thread(target=root.mainloop).start()
-
-# Now you can update the tkinter window from the main thread
-update_tkinter_window("New text message")
+fig = ica.plot_components(inst=raw_filtered, picks=range(n_components_per_page))
 
 
 # %%
-# get user input for good or bad
+# prepare the epochs
+# events = mne.find_events(
+#     raw_filtered, stim_channel="STI101", shortest_event=5, min_duration=0.002,initial_event=True
+#     )
+
+# event_dict = {
+#     "stand": 12,
+#     "travIN": 22,
+#     "travOut": 11,
+# }
+
+# epochs = mne.Epochs(raw_filtered, events, tmin=-1, tmax=3, event_id=event_dict, preload=True)
+# epochs["stand"].compute_psd(fmax= 80).plot(picks="meg", exclude="bads")
+
+# epochs_decimated = epochs.decimate(2)  # decimate to 500 Hz
 
 # %%
-# compare with the labels and record the results
-
-# %%
-# analyze the results
-
-
-
